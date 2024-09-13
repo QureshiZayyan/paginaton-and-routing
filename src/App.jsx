bc9e9817422e195d7f7ae4a9e37ac5c276f248c2
Zayyan-TSA <zayyan.tsa@gmail.com>
2024-09-13 19:43:20 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css';
-// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-// import Home from './Components/Home';
-// import Contact from './Components/Contact';
-// import About from './Components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home'; // Make sure this file exists
+import Contact from './Components/Contact'; // Make sure this file exists
+import About from './Components/About'; // Make sure this file exists
+import Navbar from './Components/Navbar';
 import Card from './Components/Card';
-
 const App = () => {
   return (
-    <>
+    <Router>
+      <Navbar />
+
       <div id="container">
         <Card />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 };
 
