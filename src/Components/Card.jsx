e157af4e79b2fe8a70f2dee41b52f39825142322
zayyan <qureshizayyan49@gmail.com>
2024-09-14 05:12:17 +0530

    d

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -31,7 +31,23 @@ const Card = () => {
           </div>
         ))
       ) : (
-        <p>No data available</p>
+        Array.from({ length: 100 }).map((_, index) => (
+          <div className="card" key={index} aria-hidden="true">
+            <div className="card-body">
+              <h5 className="card-title placeholder-glow">
+                <span className="placeholder col-6"></span>
+              </h5>
+              <p className="card-text placeholder-glow">
+                <span className="placeholder col-7"></span>
+                <span className="placeholder col-4"></span>
+                <span className="placeholder col-4"></span>
+                <span className="placeholder col-6"></span>
+                <span className="placeholder col-8"></span>
+              </p>
+              <a className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
+            </div>
+          </div>
+        ))
       )}
     </>
   );
